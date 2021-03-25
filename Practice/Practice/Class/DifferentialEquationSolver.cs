3a01a90fae80fe6d41b0e9079ad64dc1c271b27c
AndreiBelimov <nefarab@gmail.com>
2021-03-25 17:23:50 +0300

    Добавлены методы приближенного решения интегралов

---
+++ Practice/Practice/Class/DifferentialEquationSolver.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.Class
+{
+    static class DifferentialEquationSolver
+    {
+        public static Vector DifferenceMethod(Func<double, double> f, Func<double, double> g, int n, double alpha = 0, double beta = 1)
+        {
+            double h = (beta - alpha) / n;
+            var u = new double[n]; //главная диагональ
+            var a = new double[n - 1]; // верхняя диагональ
+            var c = new double[n - 1]; // нижняя диагональ
+            var y = new double[n]; //вектор значений
+
+            for (int i = 0; i < n; i++)
+            {
+                u[i] = -2 - h * h * g(alpha + h * i);
+                a[i] = 1;
+                c[i] = 1;
+                y[i] = f(alpha + h * i);
+            }
+            return SLESolver.TridiagonalMatrixAlgorithm(a, u, a, y);
+        }
+    }
+}
---
+++ Practice/Practice/Class/IntergralSolver.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.Class
+{
+    class IntergralSolver
+    {
+        public interface IIntegralSolver
+        {
+            double Solve(Func<double, double> f, double a, double b, int n);
+        }
+
+        public class RectangleSolver : IIntegralSolver
+        {
+            public double Solve(Func<double, double> f, double a, double b, int n)
+            {
+                var h = (b - a) / n;
+                double result = 0;
+
+                for (int i = 0; i < n; i++)
+                {
+                    result += f(a + i * h + h / 2) * h;
+                }
+                return result;
+            }
+        }
+        public class LeftRectangleSolver : IIntegralSolver
+        {
+            public double Solve(Func<double, double> f, double a, double b, int n)
+            {
+                var h = (b - a) / n;
+                double result = 0;
+
+                for (int i = 0; i < n; i++)
+                {
+                    result += f(a + i * h) * h;
+                }
+                return result;
+            }
+        }
+        public class RightRectangleSolver : IIntegralSolver
+        {
+            public double Solve(Func<double, double> f, double a, double b, int n)
+            {
+                var h = (b - a) / n;
+                double result = 0;
+
+                for (int i = 0; i < n; i++)
+                {
+                    result += f(a + (i + 1) * h) * h;
+                }
+                return result;
+            }
+        }
+        public class TrapezoidSolver : IIntegralSolver
+        {
+            public double Solve(Func<double, double> f, double a, double b, int n)
+            {
+                var h = (b - a) / n;
+                double result = 0;
+
+                for (int i = 0; i < n; i++)
+                {
+                    result += (f(a + i * h) + f(a + (i + 1) * h)) / 2 * h;
+                }
+                return result;
+            }
+        }
+    }
+}
